1f05553481c32711db36a265bba34cb2b86f2b9b
Austin <AustinGuo@users.noreply.github.com>
2018-03-12 14:58:15 +0900

    Update Flags.py
    Correctness: 12%
    Performance:  0%
    Task score:   6%

--- Lesson10/Flags.py
+++ Lesson10/Flags.py
@@ -6,27 +6,32 @@ def solution(A):
     N = len(A)
     P = [0] * N
 
+    if N == 1:
+        return 1
+
     if A[0] > A[1]:
         P[0] = 1
     for i in range(1, N-1):
         if A[i] > A[i-1] and A[i] > A[i+1]:
             P[i] = 1
     if A[N-1] > A[N-2]:
-        A[N-1] = 1
+        P[N-1] = 1
 
     F = 0
-    i = 2
-    while (i*i) < N:
-        Fa = L = 0
+    i = 1
+    while (i*i) <= N+1:
+        Fa = 0
+        L = i
         for p in P:
             if p == 1 and L >= i:
                 Fa += 1
                 L == 0
-                
+            if Fa == i:
+                break;
             L += 1
-            
+
         if Fa > F:
             F = Fa
         i += 1
-            
+
     return F
