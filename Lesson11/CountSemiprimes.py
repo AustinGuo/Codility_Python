79e537ba652b045615da069fd8da02a05cfab385
Austin <AustinGuo@users.noreply.github.com>
2018-03-20 17:32:40 +0900

    Update CountSemiprimes.py
    Correctness: 50%
    Performance:  0%
    Task score:  22%

--- Lesson11/CountSemiprimes.py
+++ Lesson11/CountSemiprimes.py
@@ -1,27 +1,35 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+def getPrimeFactor(n):
+    F = [0] * (n+1)
+    i = 2
+    while (i*i) <= n:
+        if F[i] == 0:
+            a = i * i
+            while a <= n:
+                if F[a] == 0:
+                    F[a] = i
+                a += i
+        i += 1
+    return F
+    
+def getNumSemi(n):
+    F = getPrimeFactor(n)
+    NS = [0] * (n+1)
+    c = 0
+    for i in range(n+1):
+        if F[i] != 0 and F[int(i/F[i])] == 0:
+            c += 1
+            NS[i] = c 
+    return NS
+    
 def solution(N, P, Q):
     # write your code in Python 3.6
-    def getPrimeFactor(n):
-        F = [0] * (n+1)
-        i = 2
-        while (i*i) <= n:
-            if F[i] == 0:
-                a = i * i
-                while a <= n:
-                    if F[a] == 0:
-                        F[a] = i
-                    a += i
-            i += 1
-        return F
-    
-    F = getPrimeFactor(N)
     M = len(P)
     R = [0] * M
+    NS = getNumSemi(N)
     for i in range(M):
-        for x in range(P[i], Q[i]+1):
-            if F[x] != 0 and F[int(x/F[x])] == 0:
-                R[i] += 1
-                
+        R[i] = NS[Q[i]] - NS[P[i-i]]
+
     return R
