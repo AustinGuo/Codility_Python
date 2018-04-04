c1bcf054005e66084f5d8d94ff82aa022b618ac1
Austin <AustinGuo@users.noreply.github.com>
2018-04-04 16:40:55 +0900

    Update CountNonDivisible.py
    Correctness: 100%
    Performance:   0%
    Task score:   55%

--- Lesson11/CountNonDivisible.py
+++ Lesson11/CountNonDivisible.py
@@ -5,11 +5,11 @@ def solution(A):
     # write your code in Python 3.6
     N = len(A)
     R = [0] * N
-    for i in range(N):
-        for j in range(N):
-            if i == j:
-                continue
+    for i in range(N-1):
+        for j in range(i+1, N):
             if A[i] % A[j] != 0:
                 R[i] += 1
+            if A[j] % A[i] != 0:
+                R[j] += 1
                 
     return R
