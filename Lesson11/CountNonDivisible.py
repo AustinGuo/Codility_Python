e1b9415d50609264c853cb81ba1642c9490f5733
Austin <AustinGuo@users.noreply.github.com>
2018-04-05 16:56:43 +0900

    Update CountNonDivisible.py
    Correctness: 100%
    Performance:   0%
    Task score:   55%

--- Lesson11/CountNonDivisible.py
+++ Lesson11/CountNonDivisible.py
@@ -1,20 +1,6 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
-# Get the smallest factor in every number(if not prime number)
-def getPrimeFactor(n):
-    F = [0] * (n+1)
-    i = 2
-    while (i*i) <= n:
-        if F[i] == 0:
-            a = i * i
-            while a <= n:
-                if F[a] == 0:
-                    F[a] = i
-                a += i
-        i += 1
-    return F
-
 # Get number count list
 def getNumList(A):
     N = len(A)*2
@@ -28,25 +14,17 @@ def solution(A):
     N = len(A)
     M = N*2
     C = getNumList(A)
-    F = getPrimeFactor(M)
     R = [N] * N
     for i in range(N):
-        R[i] -= C[1]
-        if A[i] == 1:
-            continue
-        lastfac = 0
-        x = A[i]
-        while (F[x] > 0):
-            
-            # Avoid decrease factor number count repeat
-            if F[x] != lastfac:
-                R[i] -= C[F[x]]
-                lastfac = F[x]
-            R[i] -= C[x]
-            x = int(x / F[x])
-            
-        # Check the last prime number if already be deceased.
-        if x != lastfac:
-            R[i] -= C[x]
-
+        x = 1
+        while True:
+            if A[i] % x == 0:
+                y = int(A[i]/x)
+                if x >= y:
+                    if y == x:
+                        R[i] -= C[y]
+                    break
+                R[i] -= C[x]
+                R[i] -= C[y]
+            x += 1
     return R
