f5998ccdf8a64ecc0d91d43cc77d4958723c5bf6
Austin <AustinGuo@users.noreply.github.com>
2018-04-06 11:28:06 +0900

    Update CountNonDivisible.py
    Correctness: 100%
    Performance:   0%
    Task score:   55%

--- Lesson11/CountNonDivisible.py
+++ Lesson11/CountNonDivisible.py
@@ -1,6 +1,20 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+# Get the smallest factor in every number(if not prime number)
+def getPrimeFactor(n):
+    F = [True] * (n+1)
+    F[0] = F[1] = False
+    i = 2
+    while (i*i) <= n:
+        if F[i]:
+            a = i * i
+            while a <= n:
+                F[a] = False
+                a += i
+        i += 1
+    return F
+
 # Get number count list
 def getNumList(A):
     N = len(A)*2
@@ -13,18 +27,50 @@ def solution(A):
     # write your code in Python 3.6
     N = len(A)
     M = N*2
+    F = getPrimeFactor(M)
     C = getNumList(A)
     R = [N] * N
     for i in range(N):
-        x = 1
-        while True:
-            if A[i] % x == 0:
-                y = int(A[i]/x)
-                if x >= y:
-                    if y == x:
-                        R[i] -= C[y]
-                    break
+        Flag = [True] * (M+1)
+        R[i] -= C[1]
+        if A[i] == 1:
+            continue
+        R[i] -= C[A[i]]
+        if F[A[i]]:
+            continue
+        x = 2
+        while x*x <= A[i]:
+            if not F[x] or A[i] % x != 0:
+                x += 1
+                continue
+            y = int(A[i]/x)
+            if x >= y:
+                if y == x and Flag[y]:
+                    R[i] -= C[y]
+                    Flag[y] = False
+                break
+            if Flag[x]:
                 R[i] -= C[x]
+                Flag[x] = False
+            if Flag[y]:
                 R[i] -= C[y]
+                Flag[y] = False
+            z = x * x
+            while z <= A[i]:
+                if A[i] % z == 0:
+                    y = int(A[i]/z)
+                    if z >= y:
+                        if y == z and Flag[y]:
+                            R[i] -= C[y]
+                            Flag[y] = False
+                        break
+                    if Flag[z]:
+                        R[i] -= C[z]
+                        Flag[z] = False
+                    if Flag[y]:
+                        R[i] -= C[y]
+                        Flag[y] = False
+                z += x
             x += 1
+       
     return R
