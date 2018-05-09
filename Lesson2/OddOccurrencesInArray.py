a587801f8849b97d2646394ead83ef634dd3ac14
Austin <AustinGuo@users.noreply.github.com>
2018-05-09 11:15:11 +0900

    Update OddOccurrencesInArray.py
    Task score: 100%
    Correctness: 100%
    Performance: 100%

--- Lesson2/OddOccurrencesInArray.py
+++ Lesson2/OddOccurrencesInArray.py
@@ -3,10 +3,25 @@
 
 def solution(A):
     # write your code in Python 3.6
-    L = []
+    preRes = 0
+    numCount = 0
+    
+    # Resort the list of number
+    A.sort()
+
+    # Count the same number
     for N in A:
-        if N in L:
-            L.remove(N)
+        
+        # If same as last number, plus 1
+        if preRes == N:
+            numCount += 1
+        
+        # If diffrent as last number, judge compare nubmer if odd count
+        elif numCount & 1 == 1:
+            return preRes
+        
+        # Reset compare number and count
         else:
-            L.append(N)
-    return L[0]
+            preRes = N
+            numCount = 1
+    return preRes
