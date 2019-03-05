fdc796b0459086693a5505391c4adc506e5d917f
Austin Kuo <kuoyuntony@gmail.com>
2019-03-05 09:47:25 +0900

    Update StoneWall.py
    Task score: 100%
    Correctness: 100%
    Performance: 100%

--- Lesson7/StoneWall.py
+++ Lesson7/StoneWall.py
@@ -7,22 +7,13 @@ def solution(H):
     stone_count = 1
     for i in range(1, len(H)):
         if H[i-1] > H[i]:
-            while stone_stake:
-                if stone_stake[-1] > H[i]:
-                    stone_stake.pop()
-                    stone_count += 1
-                elif stone_stake[-1] < H[i]:
-                    stone_count += 1
-                    break
-                elif stone_stake[-1] == H[i]:
-                    stone_stake.pop()
-                    stone_count += 1
+            while stone_stake and stone_stake[-1] >= H[i]:
+                stone_count += 1
+                stone = stone_stake.pop()
+                if stone == H[i]:
                     break
             else:
                 stone_count += 1
         elif H[i-1] < H[i]:
             stone_stake.append(H[i-1])
     return stone_count + len(stone_stake)
-
-H = [8,8,5,7,9,8,7,4,8]
-print(solution(H))
