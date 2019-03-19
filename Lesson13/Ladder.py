3754a643a96434a23936d81deb0add7fa0af1689
Austin Kuo <kuoyuntony@gmail.com>
2019-03-19 17:23:00 +0900

    Update Ladder.py
    Task score : 100%
    Correctness: 100%
    Performance: 100%

--- Lesson13/Ladder.py
+++ Lesson13/Ladder.py
@@ -3,13 +3,22 @@
 
 def solution(A, B):
     # write your code in Python 3.6
-    def fibonacci(n):
-        fib = [0] * (n + 1)
+    def fibonacci(n, m):
+        fib = [0] * (n + 2)
         fib[1] = 1
-        for i in range(2, n + 1):
-            fib[i] = fib[i - 1] + fib[i - 2]
-        return fib[2:]
+        a = 2 ** m
+        for i in range(2, n + 2):
+            fib[i] = (fib[i - 1] + fib[i - 2]) % a
 
-    fib_list = fibonacci(max(A)+1)
+        return fib[1:]
 
-    return list(map(lambda x,y: fib_list[x - 1] % 2 ** y, A, B))
+    def sqare(n):
+        sqr = [1] * (n + 1)
+        for i in range(1, n + 1):
+            sqr[i] = sqr[i - 1] * 2
+        return sqr
+
+    fib_list = fibonacci(max(A), max(B))
+    aqr_list = sqare(max(B))
+
+    return list(map(lambda x,y: fib_list[x] % aqr_list[y], A, B))
